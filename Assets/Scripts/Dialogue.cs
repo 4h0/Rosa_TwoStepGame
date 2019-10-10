a7a5a7a84ed3c3e93e79b1a89e71c14d6ddfc8b1
jennfrench <jennlcfrench@hotmail.com>
2019-10-10 19:55:08 -0400

    Added set pieces to scene. Created rough dialogue UI. Created dialogue scripts.

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable] // Makes this stuff visible in the inspector.
+public class Dialogue
+{
+
+    public string name; // NPC/Main character name
+
+    [TextArea(3, 10)]  // Makes the text boxes bigger in Unity inspector for visability when writing lengthier dialogue.
+    public string[] sentences; // Dialogue
+
+}
