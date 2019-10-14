bc9fe3142801d947c3cef580c648fa9bc2105e3d
Alex <heitzalex@yahoo.com>
2019-10-13 23:40:41 -0400

    Added menu system to Alex's Hangout

---
+++ Assets/AlexScripts/Menus.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menus : MonoBehaviour
+{
+    Scene CurrentScene;
+    string CurrentSceneName;
+    public void Start()
+    {
+        CurrentScene = SceneManager.GetActiveScene();
+        CurrentSceneName = CurrentScene.name;
+
+        if (CurrentSceneName != "MainMenu")
+        {
+            GameObject player = GameObject.Find("Player");
+            PlayerMenuControl player_menu_control = player.GetComponent<PlayerMenuControl>();
+        }
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("whitebox tech1");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void ResumeGame()
+    {
+        PlayerMenuControl.GamePaused = true;
+    }
+}
---
+++ Assets/AlexScripts/PlayerMenuControl.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMenuControl : MonoBehaviour
+{
+    public Transform canvas;
+    public static bool GamePaused;
+
+    void Start()
+    {
+        GamePaused = false;
+    }
+         
+    void Update() 
+    {
+        Pause();
+        Resume();
+    }
+
+    void Pause()
+    {
+        if (Input.GetKeyDown(KeyCode.Z)) {
+             
+            if (canvas.gameObject.activeInHierarchy == false) 
+            {        
+                canvas.gameObject.SetActive (true);
+                Time.timeScale = 0;
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            } 
+            else 
+            {
+                canvas.gameObject.SetActive (false);
+                Time.timeScale = 1;
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+        } 
+    }
+    public void Resume()
+    {
+        if (GamePaused == true)
+        {
+            canvas.gameObject.SetActive (false);
+            Time.timeScale = 1;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+
+            GamePaused = false;
+        }
+    }
+ 
+}
