05a9564036d505893a60ecb2694df11e42bc54cf
Quira Christine <quira.christine@gmail.com>
2019-10-13 21:45:20 -0400

    playing around with whitebox art scene

---
+++ Assets/Scripts/Rotation_Watermill.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation_Watermill : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(Time.deltaTime*50,0,0));
+    }
+}
