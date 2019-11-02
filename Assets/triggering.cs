2c87a7d800eae663c7af7d13ae451f217c3dfa87
Magoo763 <37201821+Magoo763@users.noreply.github.com>
2019-11-02 10:17:51 -0400

    activity action

---
+++ Assets/triggering.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggering : MonoBehaviour
+{
+    public int h=0;
+    private GameObject door;
+    // Start is called before the first frame update
+    private Vector3 target=new Vector3(1097, 263, -80);
+    
+
+    void OnTriggerEnter(Collider col)
+
+
+    {
+        if (col.gameObject.name=="cube")
+        {
+
+            Debug.Log("hello");
+            h = 1;
+            Debug.Log(h);
+        }
+    }
+
+    void Start()
+    {
+
+        door = GameObject.Find("door");
+        
+
+
+
+
+    }
+
+   
+
+
+    void Update()
+    {
+if (h==1)
+        {
+            float speed = 10;
+            Debug.Log("yellow");
+            //door.transform.Translate((Vector3.forward* 9)*(Time.deltaTime));
+            door.transform.position = Vector3.MoveTowards(door.transform.position, target, speed * Time.deltaTime);
+            
+
+        }
+
+
+    }
+
+   
+
+    
+}
