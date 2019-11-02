9ed4704bbac38b653609d6505550296387a54232
Magoo763 <37201821+Magoo763@users.noreply.github.com>
2019-11-02 14:15:08 -0400

    made extra requiremenrs

---
+++ Assets/Scripts/triggering.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggering : MonoBehaviour
+{
+    public int h=0;
+    private GameObject door;
+    // Start is called before the first frame update
+    private Vector3 target=new Vector3(1097, 263, 50);
+    private GameObject box;
+
+    void OnTriggerEnter(Collider col)
+
+
+    {
+        if (col.gameObject.name=="cube")
+        {
+
+            Debug.Log("hello");
+            h = 1;
+            Debug.Log(h);
+        }
+    }
+
+    void Start()
+    {
+
+        door = GameObject.Find("door");
+        box = GameObject.Find("box");
+
+
+
+
+
+    }
+
+   
+
+
+    void Update()
+    {
+        if (h == 1)
+
+        {
+            if (box.transform.position.z <= -38 && box.transform.position.z >= -94)
+
+            {
+
+                if (box.transform.position.x >= 273 && box.transform.position.x <= 310)
+                { 
+
+                float speed = 40;
+                Debug.Log("yellow");
+
+                door.transform.position = Vector3.MoveTowards(door.transform.position, target, speed * Time.deltaTime);
+            }
+        }
+
+        }
+
+
+    }
+
+
+
+    //Debug.Log("done");
+    // if(box.transform.position.z>=-38 && box.transform.position.z<=-94)
+    // {
+
+    //  if (box.transform.position.x>=267 && box.transform.position.x<=311)
+    //  {
+    //         Debug.Log("done");
+    //         float speed = 40;
+    //door.transform.position = Vector3.MoveTowards(door.transform.position, target.transform.position, speed);
+
+    //   }
+
+    // }
+
+}
