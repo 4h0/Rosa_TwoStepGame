3646411fc9f5858f4f730e15666441335aeeb62b
4h0 <35616167+4h0@users.noreply.github.com>
2019-11-11 12:52:06 -0500

    puzzle 1 temp (still need debug)

--- Assets/Tech Team/Scripts/AlexScripts/StoneLogic.cs
+++ Assets/Tech Team/Scripts/AlexScripts/StoneLogic.cs
@@ -5,15 +5,32 @@ using UnityEngine;
 public class StoneLogic : MonoBehaviour
 {
     private Rigidbody stoneRigidBody;
+    private Puzzle1_Khoa puzzle1Reference;
+
+    private bool doOnce;
 
     private void Awake()
-    {
+    {       
         stoneRigidBody = GetComponent<Rigidbody>();
+        puzzle1Reference = FindObjectOfType<Puzzle1_Khoa>();
+
+        doOnce = false;
     }
 
+    private void Update()
+    {
+        Debug.Log(doOnce);
+    }
+
+    private void Puzzle1Start()
+    {
+        doOnce = true;
+        puzzle1Reference.ChangeDirection();
+    }
+    
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.tag =="Player")
+        if(collision.gameObject.tag == "Player")
         {
             stoneRigidBody.constraints = RigidbodyConstraints.FreezeAll;
         }
@@ -23,6 +40,22 @@ public class StoneLogic : MonoBehaviour
         if(collision.gameObject.tag == "Player")
         {
             stoneRigidBody.constraints = RigidbodyConstraints.None;
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Puzzle1Trigger" && !doOnce)
+        {
+            Puzzle1Start();
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Puzzle1Trigger")
+        {
+            doOnce = false;
         }
     }
 }
---
+++ Assets/Tech Team/Scripts/KhoaScripts/Puzzle1_Khoa.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle1_Khoa : MonoBehaviour
+{
+    public Transform destination;
+
+    private Rigidbody doorRigidbody;
+    private Transform originalPosition;
+    private Transform movetoDestination;
+
+    private bool canChange;
+    private int turnBack;
+    private int multiplier;
+
+    private void Awake()
+    {
+        doorRigidbody = GetComponent<Rigidbody>();
+
+        originalPosition = this.transform;
+        movetoDestination = originalPosition;
+
+        turnBack = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        if(canChange)
+        {
+            doorRigidbody.AddForce(transform.right * multiplier * Time.deltaTime * 300, ForceMode.Acceleration); 
+        }
+        else
+        {
+            doorRigidbody.velocity = new Vector3(0, 0, 0);
+        }
+    }
+
+    IEnumerator CanMove()
+    {
+        canChange = true;
+
+        yield return new WaitForSeconds(3f);
+
+        canChange = false;
+    }
+
+    public void ChangeDirection()
+    {
+        turnBack++;
+        StartCoroutine(CanMove());
+
+        if (turnBack % 2 == 0)
+        {
+            multiplier = -1;
+        }
+        else
+        {
+            multiplier = 1;
+        }
+    }
+}
