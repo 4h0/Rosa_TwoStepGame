68f8197a19568bf7c28be11a48dd8c6e6859bbac
Magoo763 <37201821+Magoo763@users.noreply.github.com>
2019-11-11 08:50:09 -0500

    puzzle 2 done

---
+++ Assets/Colors.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Colors : MonoBehaviour
+{
+    public Material red;
+    public Material blue;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Renderer>().material = blue;
+
+
+    }
+    void OnTriggerEnter(Collider call)
+    {
+
+        if (call.gameObject.name == "Player")
+        {
+            GetComponent<Renderer>().material = red;
+            Debug.Log("yos");
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
