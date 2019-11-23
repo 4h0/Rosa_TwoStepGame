8cd2f76f38443e1113e02479ee893484ff05fa5d
Magoo763 <37201821+Magoo763@users.noreply.github.com>
2019-11-23 11:45:24 -0500

    ed

--- Assets/Tech Team/Scripts/JennsScripts/dialogueTrigger.cs
+++ Assets/Tech Team/Scripts/JennsScripts/dialogueTrigger.cs
@@ -7,19 +7,26 @@ public class dialogueTrigger : MonoBehaviour
 {
     public Flowchart flowchart; // calls the flowchart.
     private bool hasPlayer; // is the player in a collider? yes or no
-
+    private PauseMenuController_Khoa pauseMenuReference;
+    public int questType;
+    public GameObject[] taskRelatedGameObject;
+    private void Awake()
+    {
+        pauseMenuReference = FindObjectOfType<PauseMenuController_Khoa>();
+        questType = flowchart.GetIntegerVariable("questType");
+    }
     private void Update()
     {
         if (hasPlayer && Input.GetKeyDown("k")) //is hasPlayer true or false? if it's true and key pressed then
         {
 
-            if (this.gameObject.tag == "NPC")
+            if (this.gameObject.tag == "NPC") // checks to see if the radius is tagged with the NPC tag.
             {
                 Debug.Log("NPC1");
                 flowchart.ExecuteBlock("Quest Dialogue"); // we execute the named block within the flowchart.
             }
 
-            else if (this.gameObject.tag == "NPC2")
+            else if (this.gameObject.tag == "NPC2") // checks tag.
             {
                 Debug.Log("Npc2");
                 flowchart.ExecuteBlock("Testing1"); // we execute the named block within the flowchart.
@@ -27,21 +34,27 @@ public class dialogueTrigger : MonoBehaviour
 
             else if (this.gameObject.tag == "NPC3")
             {
-                Debug.Log("Npc2");
+                Debug.Log("Npc3");
                 flowchart.ExecuteBlock("FlavorHW"); // we execute the named block within the flowchart.
             }
 
 
             else if (this.gameObject.tag == "NPC4")
             {
-                Debug.Log("Npc2");
+                Debug.Log("Npc4");
                 flowchart.ExecuteBlock("FlavorC"); // we execute the named block within the flowchart.
             }
 
             else if (this.gameObject.tag == "NPC5")
             {
-                Debug.Log("Npc2");
+                Debug.Log("Npc5");
                 flowchart.ExecuteBlock("FlavorG"); // we execute the named block within the flowchart.
+            }
+
+            else if (this.gameObject.tag == "NPC6")
+            {
+                Debug.Log("NPC6");
+                flowchart.ExecuteBlock("fireQuest"); // executing the fire quest chain.
             }
 
         }
@@ -63,6 +76,20 @@ public class dialogueTrigger : MonoBehaviour
         if (other.CompareTag("Player")) // checks for the player tag
         {
             hasPlayer = false; // sets hasPlayer to be false so dialogue won't play.
+        }
+    }
+    private void TaskAgree()
+    {
+        TurnOnTaskObjects();
+
+        pauseMenuReference.AddToOngoingList(questType);
+    }
+
+        private void TurnOnTaskObjects()
+    {
+        for (int counter = 0; counter < taskRelatedGameObject.Length; counter++)
+        {
+            taskRelatedGameObject[counter].SetActive(true);
         }
     }
 }
