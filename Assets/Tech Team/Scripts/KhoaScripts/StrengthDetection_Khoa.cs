b5449bdd3bce6a574c98eda8c8c4f40c99da395c
4h0 <35616167+4h0@users.noreply.github.com>
2019-12-08 02:22:36 -0500

    Revert "Revert "Strength Debug""
    This reverts commit b5790ed4c599b5751b753db818313b2a8e2a8a31.

--- Assets/Tech Team/Scripts/KhoaScripts/StrengthDetection_Khoa.cs
+++ Assets/Tech Team/Scripts/KhoaScripts/StrengthDetection_Khoa.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class StrengthDetection_Khoa : MonoBehaviour
 {
+    private PlayerController_Alex playerReference;
+
+    private void Awake()
+    {
+        playerReference = FindObjectOfType<PlayerController_Alex>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "PickUp")
         {
             this.transform.parent.GetChild(0).GetComponent<Quest2_Khoa>().StopSideQuest2();
+            playerReference.strengthEnd = true;
         }
     }
 }
